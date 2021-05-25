12cd4a4a401ab374092118159a2948b2bc18978a
ing.conti <ingconti@ingconti.com>
2021-05-25 07:46:16 +0200

    added fake tests.

---
+++ src/test/java/MyApp/AppTest.java
@@ -0,0 +1,16 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
